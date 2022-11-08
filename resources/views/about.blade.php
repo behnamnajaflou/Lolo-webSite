aa05a7204dc737297c008e5beebb949c0f03ee23
ben <ben@yahoo.com>
2022-11-08 16:25:34 +0100

    lolo-start

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.app')
+@section('content')
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container">
+                <div class="masthead-subheading">About</div>
+                <div class="masthead-heading text-uppercase">In this page you will know about us in detail</div>
+                <a class="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
+            </div>
+        </header>
+        <!-- Services-->
+       
+@endsection
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,14 @@
+
+@extends('layout.app')
+@section('content')
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container">
+                <div class="masthead-subheading">Contact</div>
+                <div class="masthead-heading text-uppercase">In this page you can contact us</div>
+                <a class="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
+            </div>
+        </header>
+        <!-- Services-->
+       
+@endsection
